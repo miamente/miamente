cd25f9fe82eb2bbe4c7edfd84934fc1ca771089f
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-09-19 19:05:57 -0500

    refactor: remove dotenv loading from Alembic environment

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,9 +5,7 @@ from sqlalchemy import pool
 from alembic import context
 import os
 import sys
-from dotenv import load_dotenv
 
-load_dotenv()
 # Add the app directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
